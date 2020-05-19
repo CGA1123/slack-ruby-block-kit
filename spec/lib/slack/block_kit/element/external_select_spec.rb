95f64a38ab3ff84800be3b57da85ae413b084a70
Yoshinori Kawasaki <luvtechno@users.noreply.github.com>
2020-05-19 23:59:40 +0900

    Fix ExternalSelect#as_json (#19)
    * Fix ExternalSelect#as_json
    
    * Be compliant with rubocop

--- spec/lib/slack/block_kit/element/external_select_spec.rb
+++ spec/lib/slack/block_kit/element/external_select_spec.rb
@@ -3,5 +3,32 @@
 require 'spec_helper'
 
 RSpec.describe Slack::BlockKit::Element::ExternalSelect do
-  pending
+  let(:instance) { described_class.new(**params) }
+  let(:placeholder_text) { 'some text' }
+  let(:action_id) { 'my-action' }
+  let(:params) do
+    {
+      placeholder: placeholder_text,
+      action_id: action_id
+    }
+  end
+
+  describe '#as_json' do
+    subject(:as_json) { instance.as_json }
+
+    let(:expected_json) do
+      {
+        type: 'external_select',
+        placeholder: {
+          'type': 'plain_text',
+          'text': placeholder_text
+        },
+        action_id: action_id
+      }
+    end
+
+    it 'correctly serializes' do
+      expect(as_json).to eq(expected_json)
+    end
+  end
 end

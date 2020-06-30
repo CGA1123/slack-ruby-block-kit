26eab5a99dc55f9994a4a136aecff4a04a520a56
anvox <thien.an.vo.nguyen@gmail.com>
2020-06-30 13:26:28 +0700

    Add unit tests

--- spec/lib/slack/block_kit_spec.rb
+++ spec/lib/slack/block_kit_spec.rb
@@ -21,5 +21,127 @@ RSpec.describe Slack::BlockKit do
     end
   end
 
-  # describe '.message'
+  describe '.message' do
+    let(:expected_json) do
+      {
+        channel: '__CHANNEL_ID__',
+        blocks: [
+          {
+            alt_text: '__ALT_TEXT__',
+            image_url: 'https://image.com/image.png',
+            type: 'image'
+          }
+        ]
+      }
+    end
+
+    it 'correctly serializes' do
+      msg = described_class.message(channel: '__CHANNEL_ID__')
+      msg.blocks.image(url: 'https://image.com/image.png', alt_text: '__ALT_TEXT__')
+
+      expect(msg.as_json).to eq(expected_json)
+    end
+  end
+
+  describe '.home' do
+    let(:expected_json) do
+      {
+        type: 'home',
+        blocks: [
+          {
+            alt_text: '__ALT_TEXT__',
+            image_url: 'https://image.com/image.png',
+            type: 'image'
+          }
+        ]
+      }
+    end
+
+    it 'correctly serializes' do
+      home = described_class.home
+      home.blocks.image(url: 'https://image.com/image.png', alt_text: '__ALT_TEXT__')
+
+      expect(home.as_json).to eq(expected_json)
+    end
+
+    context 'all options' do
+      let(:expected_json) do
+        {
+          type: 'home',
+          blocks: [
+            {
+              alt_text: '__ALT_TEXT__',
+              image_url: 'https://image.com/image.png',
+              type: 'image'
+            }
+          ],
+          private_metadata: '__META_DATA__',
+          callback_id: '__CALLBACK_ID__',
+          external_id: '__EXTERNAL_ID__'
+        }
+      end
+
+      it 'correctly serializes' do
+        home = described_class.home(private_metadata: '__META_DATA__',
+                                    callback_id: '__CALLBACK_ID__',
+                                    external_id: '__EXTERNAL_ID__')
+        home.blocks.image(url: 'https://image.com/image.png', alt_text: '__ALT_TEXT__')
+
+        expect(home.as_json).to eq(expected_json)
+      end
+    end
+  end
+
+  describe '.modal' do
+    let(:expected_json) do
+      {
+        type: 'modal',
+        blocks: [
+          {
+            alt_text: '__ALT_TEXT__',
+            image_url: 'https://image.com/image.png',
+            type: 'image'
+          }
+        ]
+      }
+    end
+
+    it 'correctly serializes' do
+      modal = described_class.modal
+      modal.blocks.image(url: 'https://image.com/image.png', alt_text: '__ALT_TEXT__')
+
+      expect(modal.as_json).to eq(expected_json)
+    end
+
+    context 'all options' do
+      let(:expected_json) do
+        {
+          type: 'modal',
+          blocks: [
+            {
+              alt_text: '__ALT_TEXT__',
+              image_url: 'https://image.com/image.png',
+              type: 'image'
+            }
+          ],
+          private_metadata: '__META_DATA__',
+          callback_id: '__CALLBACK_ID__',
+          external_id: '__EXTERNAL_ID__',
+          clear_on_close: true,
+          notify_on_close: true
+        }
+      end
+
+      it 'correctly serializes' do
+        modal = described_class.modal(private_metadata: '__META_DATA__',
+                                     callback_id: '__CALLBACK_ID__',
+                                     external_id: '__EXTERNAL_ID__',
+                                     clear_on_close: true,
+                                     notify_on_close: true)
+        modal.blocks.image(url: 'https://image.com/image.png', alt_text: '__ALT_TEXT__')
+
+        expect(modal.as_json).to eq(expected_json)
+      end
+    end
+  end
 end

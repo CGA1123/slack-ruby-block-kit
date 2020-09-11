75008be7bb59b60848597a944319ff697dc62814
Albert Salim <caalberts@users.noreply.github.com>
2020-09-11 23:11:36 +0800

    Implement Slack::BlockKit::Element::Checkboxes (#44)

---
+++ lib/slack/block_kit/element/checkboxes.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module Slack
+  module BlockKit
+    module Element
+      # A checkbox group that allows a user to choose multiple items from
+      # a list of possible options.
+      #
+      # https://api.slack.com/reference/messaging/block-elements#checkboxes
+      class Checkboxes
+        TYPE = 'checkboxes'
+
+        attr_accessor :confirm
+
+        def initialize(action_id:)
+          @action_id = action_id
+          @options = []
+          @initial_options = []
+          @confirm = nil
+
+          yield(self) if block_given?
+        end
+
+        def option(value:, text:, description: nil)
+          @options << Composition::Option.new(
+            value: value,
+            text: text,
+            description: description
+          )
+
+          self
+        end
+
+        def initial(value:, text:, description: nil)
+          @initial_options << Composition::Option.new(
+            value: value,
+            text: text,
+            description: description
+          )
+
+          self
+        end
+
+        def confirmation_dialog
+          @confirm = Composition::ConfirmationDialog.new
+
+          yield(@confirm) if block_given?
+
+          @confirm
+        end
+
+        def as_json(*)
+          {
+            type: TYPE,
+            action_id: @action_id,
+            options: @options.map(&:as_json),
+            initial_options: @initial_options.any? ? @initial_options.map(&:as_json) : nil,
+            confirm: @confirm&.as_json
+          }.compact
+        end
+      end
+    end
+  end
+end

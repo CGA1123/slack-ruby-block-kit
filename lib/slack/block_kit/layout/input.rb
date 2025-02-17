38c76c523a296f82ecf5421cdf37be0938373298
070bex424pankaj <pankaj.dhakal001@gmail.com>
2025-02-17 18:19:12 +0545

    refactor arguments pass

--- lib/slack/block_kit/layout/input.rb
+++ lib/slack/block_kit/layout/input.rb
@@ -218,22 +218,21 @@ module Slack
 
         def number_input(
           action_id:,
+          is_decimal_allowed: nil,
           placeholder: nil,
           initial_value: nil,
-          is_decimal_allowed: nil,
-          focus_on_load: nil,
-          dispatch_action_config: nil,
           min_value: nil,
-          max_value: nil
+          max_value: nil,
+          focus_on_load: nil,
         )
           @element = Element::NumberInput.new(
             action_id: action_id,
+            is_decimal_allowed: is_decimal_allowed,
             placeholder: placeholder,
             initial_value: initial_value,
-            is_decimal_allowed: is_decimal_allowed,
-            focus_on_load: focus_on_load,
             min_value: min_value,
             max_value: max_value
+            focus_on_load: focus_on_load,
           )
 
           self
